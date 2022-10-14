2259ab0804dcd98ea8141dd8f182971ee4a37c6b
pforerol <pforerol@gmail.com>
2022-10-13 19:34:54 -0500

    First update 13Oct

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+ 
+let UserSchema = new mongoose.Schema({
+    id: Number,
+    nombre: String,
+    apellidos: String,
+    ciudad: String,
+    correo: String,
+    telefono: String,
+    password: String
+})
+ 
+module.exports = mongoose.model('usuario', UserSchema, 'usuario')
